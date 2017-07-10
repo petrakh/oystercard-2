d9aec3ed0e7fb6374352d0a52e6fa4e80de201fe
Cong Cong Bo <cong@hippocampus.ai>
2017-07-10 17:44:29 +0100

    complete challenge #04: added balance instance variable

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 ruby '2.4.1'
 
 group :development, :test do
-  gem "rspec"
+  gem 'rspec'
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  describe '#balance' do
+    it 'shows balance of 0 for newly initiated card' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
