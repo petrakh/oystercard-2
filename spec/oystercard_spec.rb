47095d1d05caf8a32ed93b94fa5ef8c516143ebf
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-11 13:56:49 +0200

    complete challenge #05: added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,10 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe '#top_up' do
+    it 'tops up the balance when requested' do
+      expect { subject.top_up 5 }.to change { subject.balance }.by 5
+    end
+  end
 end
