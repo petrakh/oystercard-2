4481a7ba8762f58338306e09374fbc718755e07b
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-11 17:40:28 +0200

    complete challenge #08: in_journey instance variable added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,11 @@
 class Oystercard
   MAX_BALANCE = 90
   attr_reader :balance
+  attr_accessor :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -13,5 +15,13 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,20 +7,34 @@ describe Oystercard do
     end
   end
 
-  describe '#top_up' do
+  describe '#change_balance' do
+    before(:each) { subject.top_up(Oystercard::MAX_BALANCE) }
     it 'tops up the balance when requested' do
-      expect { subject.top_up 5 }.to change { subject.balance }.by 5
+      expect(subject.balance).to eq Oystercard::MAX_BALANCE
     end
 
     it 'sets maximum limit of £90' do
-      expect { subject.top_up Oystercard::MAX_BALANCE + 1 }.to raise_error "You tried to increase your balance by #{Oystercard::MAX_BALANCE + 1}. This is impossible! The maximum limit is £#{Oystercard::MAX_BALANCE}!"
+      expect { subject.top_up 1 }.to raise_error "You tried to increase your balance by #{Oystercard::MAX_BALANCE + 1}. This is impossible! The maximum limit is £#{Oystercard::MAX_BALANCE}!"
+    end
+
+    it 'deducts amount from card' do
+      expect { subject.deduct 5 }.to change { subject.balance }.by -5
     end
   end
 
-  describe '#deduct' do
-    it 'deducts amount from card' do
-      subject.top_up(20)
-      expect { subject.deduct 5 }.to change { subject.balance }.by -5
+  describe '#journey' do
+    it 'starts with in_journey false value upon initialization' do
+      expect(subject.in_journey).to eq false
+    end
+
+    it 'changes in_journey to true with touch_in' do
+      expect { subject.touch_in }.to change { subject.in_journey }.to true
+    end
+
+    it 'changes in_journey to false with touch_out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey).to eq false
     end
   end
 end
