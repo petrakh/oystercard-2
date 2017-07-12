f5c0d4e98c09d0e5247c5f18495927f623e66a5d
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-12 13:32:35 +0200

    complete challenge #11: entry station variable added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,27 +1,33 @@
+# this will become our main class for the Oystrcard challenge
 class Oystercard
   MAX_BALANCE = 90
   MIN_FARE = 1
-  attr_reader :balance
-  attr_accessor :in_journey
+  attr_reader :balance, :entry_station
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station
   end
 
   def top_up(amount)
-    fail "You tried to increase your balance by #{MAX_BALANCE + 1}. This is impossible! The maximum limit is £#{MAX_BALANCE}!" if balance + amount > MAX_BALANCE
+    raise "You tried to increase your balance by #{MAX_BALANCE + 1}. This is impossible! The maximum limit is £#{MAX_BALANCE}!" if balance + amount > MAX_BALANCE
     @balance += amount
   end
 
-  def touch_in
-    fail "Insufficient funds!" if balance < MIN_FARE
+  def touch_in(station_name)
+    raise "Insufficient funds!" if balance < MIN_FARE
     @in_journey = true
+    @entry_station = station_name
   end
 
   def touch_out
     @in_journey = false
     deduct(MIN_FARE)
+    @entry_station = nil
+  end
+
+  def in_journey?
+    !!entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,49 +1,65 @@
 require 'oystercard'
 
 describe Oystercard do
-  describe '#balance' do
+  let(:station) {double :station}
+
+  describe '#initialize' do
     it 'shows balance of 0 for newly initiated card' do
       expect(subject.balance).to eq 0
     end
+
+    it 'starts with in_journey false value upon initialization' do
+      expect(subject).not_to be_in_journey
+    end
   end
 
-  describe '#change_balance' do
+  describe '#top_up' do
     before(:each) { subject.top_up(Oystercard::MAX_BALANCE) }
+
     it 'tops up the balance when requested' do
       expect(subject.balance).to eq Oystercard::MAX_BALANCE
     end
 
     it 'sets maximum limit of £90' do
-      expect { subject.top_up 1 }.to raise_error "You tried to increase your balance by #{Oystercard::MAX_BALANCE + 1}. This is impossible! The maximum limit is £#{Oystercard::MAX_BALANCE}!"
+      expect { subject.top_up Oystercard::MIN_FARE }.to raise_error "You tried to increase your balance by #{Oystercard::MAX_BALANCE + Oystercard::MIN_FARE}. This is impossible! The maximum limit is £#{Oystercard::MAX_BALANCE}!"
     end
-
   end
 
-  describe '#journey' do
-    it 'starts with in_journey false value upon initialization' do
-      expect(subject.in_journey).to eq false
+  describe '#touch_in' do
+    before(:each) {subject.top_up(Oystercard::MAX_BALANCE)}
+    before(:each) {subject.touch_in(station)}
+
+    it 'changes in_journey to true with touch_in' do
+      expect(subject).to be_in_journey
+    end
+
+    it 'assigns station name on touch_in' do
+      expect(subject.entry_station).not_to be_nil
     end
 
     it 'doesnt allow touch_in unless the card has a minimum balance' do
-      expect {subject.touch_in}.to raise_error "Insufficient funds!"
+      allow(subject).to receive(:balance) {0}
+      expect {subject.touch_in(station)}.to raise_error "Insufficient funds!"
     end
+  end
 
-    it 'changes in_journey to true with touch_in' do
-      subject.top_up(Oystercard::MAX_BALANCE)
-      expect { subject.touch_in }.to change { subject.in_journey }.to true
-    end
+
+  describe '#touch_out' do
+    before(:each) { subject.top_up(Oystercard::MAX_BALANCE)}
+    before(:each) { subject.touch_in(station)}
 
     it 'changes in_journey to false with touch_out' do
-      subject.top_up(Oystercard::MAX_BALANCE)
-      subject.touch_in
       subject.touch_out
-      expect(subject.in_journey).to eq false
+      expect(subject).not_to be_in_journey
     end
 
     it "deducts the minimum fare on touch_out" do
-      subject.top_up(10)
-      subject.touch_in
       expect{ subject.touch_out }.to change{ subject.balance }.by (-Oystercard::MIN_FARE)
+    end
+
+    it 'forgets station name on touch_out' do
+      subject.touch_out
+      expect(subject.entry_station).to be_nil
     end
   end
 end
